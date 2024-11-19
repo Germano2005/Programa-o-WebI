fb64532e254b0d8b1bdb7252cb19f01e08b965f9
Germano <germano.dc@aluno.ifsc.edu.br>
2024-11-19 17:09:25 -0300

    Commit

---
+++ L3/exerc2/php/script.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Fundamentos da linguagem PHP </title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+<h1> Fundamentos do PHP - listaL3 - exercício 2 - resposta do PHP </h1>
+<p></p>
+<?php
+// recebendo os dados do formulário e guardando em três variáveis simples
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+
+$aluno1 = $_POST['nome1'];
+$aluno2 = $_POST['nome2'];
+$aluno3 = $_POST['nome3'];
+
+//va,os criar o vetor, usando o nome de cada aluno com o índice associativo
+$vetorAlunos = [$aluno1 => $nota1, $aluno2 => $nota2, $aluno3 => $nota3];
+
+echo "<table> 
+<caption> Relação de dados dos alunos do CDTS/PRW1</caption>
+    <tr> 
+        <th>Nome</th>
+        <th>Nota</th>
+        
+    </tr>";
+
+//para compormos o corpo da tabela, necessitamos navegar por todos o vetor e extrair seus dados
+foreach ($vetorAlunos as $aluno => $nota){
+    echo " <tr>
+          <td>$aluno</td>
+          <td>$nota</td>
+          </tr>";
+}
+
+echo "</table>";
+
+$notaMaior = max($vetorAlunos);
+$melhorAluno = array_search($notaMaior, $vetorAlunos);
+echo "<p> A maior nota é <span>$notaMaior</span> do aluno <span>$melhorAluno</span></p>";
+
+?>
+</body>
+</html>
